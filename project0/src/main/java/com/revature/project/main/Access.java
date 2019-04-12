6a284a72cb283fe3d705ba95b38690de25a92172
Em <ebaillie27@gmail.com>
2019-04-12 04:17:06 -0400

    For the Hobbitsis

--- project0/src/main/java/com/revature/project/main/Access.java
+++ project0/src/main/java/com/revature/project/main/Access.java
@@ -3,6 +3,7 @@ package com.revature.project.main;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 //import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -46,46 +47,6 @@ public class Access {
 		}
 	}
 		
-		//throws UserNotFoundException, NumberFormatException, IOException {
-//		BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
-//		System.out.println("type here fool");
-//		
-//		int accountId = Integer.parseInt(sc.readLine());
-//		
-//		double deposit = Double.parseDouble(sc.readLine());
-//		AccountDAOImpl acd = new AccountDAOImpl();
-//		
-//		acd.updateAccountByDeposit(accountId, deposit);
-//		Scanner scan = new Scanner(System.in);
-//		int userId = scan.nextInt();
-//		String accountType = scan.nextLine();
-		
-		//acd.deleteAccount(userId);
-		
-		//acd.createAccount(userId, accountType);
-//		scan.close();
-		
-		
-		
-		
-//		List<Accounts> accountsList = acd.getAccounts();
-//		for(Accounts a: accountsList) {
-//	
-//		System.out.println(a);
-//		}
-		
-//
-//		AccountDAOImpl acd = new AccountDAOImpl();
-//		int userId = 9999;
-//		
-//			try {
-//				System.out.println(acd.getCurrentBalance(userId));
-//			} catch (AccountNotFoundException e) {
-//				
-//				e.printStackTrace();
-//			}
-//			
-
 	
 	public static void entry(BufferedReader scan) throws UserNotFoundException, NumberFormatException, IOException {
 		Sections.firstSection();
@@ -123,32 +84,21 @@ public class Access {
 		};
 	}
 
-//	public static void getUserAccounts(BufferedReader sc) throws NumberFormatException, IOException {
-//		System.out.println("Please Enter Username and Password");
-//		String username = sc.readLine();
-//		int password = Integer.parseInt(sc.readLine());
-//		if (!username.isEmpty() && password > 0) {
-//		acd.getUserAccountsByLogin(username, password);
-//		}
-//	}
-	
-
-
 	private static void userMainMenu(BufferedReader scan) throws NumberFormatException, IOException {
 		Sections.thirdSection();
 		int txt = Integer.parseInt(scan.readLine());
 	    switch (txt) {
 	    case 1://go to savings account
 	    	System.out.println("You have selected Savings Account");
-	    	goToSavings(scan);
+	    	//goToSavings(scan);
 	    	break;
 	    case 2://go to checking account
 	    	System.out.println("You have selected Checking Account");
-	    	goToChecking(scan);
+	    	//goToChecking(scan);
 	    	break;
 	    case 3://make a new account
 	    	System.out.println("You have selected to make a New Account");
-	    	createAccount(scan);
+	    	goCreateAccount(scan);
 	    	break;
 	    case 4: //exit banking application
 	    	System.out.println("You have selected to Exit the Application. Goodbye!");
@@ -159,141 +109,133 @@ public class Access {
 	    }
 		
 	}
-}
-//	
-//	//User Selected Methods
-//	
-//	//balance 
-//	
-//	
-//	
-//	
-//	
-	private static void goToSavings() {
-		Sections.fourthSection();
-		
-		System.out.println("YOUR CURRENT BALANCE IS: "+balance);//need to write code for balance
-		int txt = scan.nextInt();
-	    switch (txt) {
-	    case 1://deposit
-	    	System.out.println("You have selected to Make a Deposit");
-	    	goToSavings();
-	    	break;
-	    case 2://withdraw
-	    	System.out.println("You have selected to Make a Withdrawl");
-	    	goToChecking();
-	    	break;
-	    case 3://transaction history
-	    	System.out.println("You have selected to Review Transaction History");
-	    	createAccount();
-	    	break;
-	    case 4: //close account
-	    	System.out.println("You have selected to Close an Account");
-	    	break;
-	    case 5://main menu
-	    	System.out.println("You have selected to Return to Main Menu");
-	    	userMainMenu(null); //-----------------------------------------------need to redirect with actual param
-	    	break;
-	    case 6://exit 
-	    	System.out.println("You have selected to Exit the Application. Goodbye!");
-	    	break;
-	    default:
-	    	System.out.println("Please select a number 1 through 4");
-	    }
-		
-	}
-	
-	private static void goToChecking() {
-		Sections.fifthSection();
-		balance = get
-				
-		System.out.println("YOUR CURRENT BALANCE IS: "+balance);//need to write code for balance
-		int txt = scan.nextInt();
-	    switch (txt) {
-	    case 1://deposit
-	    	System.out.println("You have selected to Make a Deposit");
-	    	goToSavings();
-	    	break;
-	    case 2://withdraw
-	    	System.out.println("You have selected to Make a Withdrawl");
-	    	goToChecking();
-	    	break;
-	    case 3://transaction history
-	    	System.out.println("You have selected to Review Transaction History");
-	    	createAccount();
-	    	break;
-	    case 4: //close account
-	    	System.out.println("You have selected to Close an Account");
-	    	break;
-	    case 5://main menu
-	    	System.out.println("You have selected to Return to Main Menu");
-	    	userMainMenu(null); //-----------------------------------------------need to redirect with actual param
-	    	break;
-	    case 6://exit 
-	    	System.out.println("You have selected to Exit the Application. Goodbye!");
-	    	break;
-	    default:
-	    	System.out.println("Please select a number 1 through 4");
-	    }
-	}
-	
-	private static void createAccount() {
-		Sections.sixthSection();
-		int txt = scan.nextInt();
-	    switch (txt) {
-	    case 1://create new savings account
-	    	System.out.println("You have selected to Create a New Savings Account");
-	    	goToSavings();
-	    	break;
-	    case 2://create new checking account
-	    	System.out.println("You have selected to Create a New Checking Account");
-	    	goToChecking();
-	    	break;
-	    case 3://return to main menu
-	    	System.out.println("You have selected to Return to Main Menu");
-	    	userMainMenu(null); //---------------------------------------------------------------another to redirect wiht actual param
-	    	break;
-	    case 4: //exit banking application
-	    	System.out.println("You have selected to Exit the Application. Goodbye!");
-	    	break;
-	    default:
-	    	System.out.println("Please select a number 1 through 4");
-	    }
-	}
 
+	private static void goCreateAccount(BufferedReader scan) throws NumberFormatException, IOException {
+		System.out.println("Please Enter Your UserId");
+    	int userId = Integer.parseInt(scan.readLine());
+    	System.out.println("Enter Either Savings or Checking Type");
+    	String typeId = scan.readLine();
+    	if (typeId.equals("Savings") || typeId.equals("Checking")) {
+    		acd.createAccount(userId, typeId);
+    		System.out.println("New Account Generated");
+    		System.out.println("TYPE 0 TO RETURN TO MENU");
+	    	int txt1 = Integer.parseInt(scan.readLine());
+	    		if (txt1 == 0) {
+	    			userMainMenu(scan);
+	    		}
+    	} else {
+    		System.out.println("Invalid Account Type - Try Again");
+    		goCreateAccount(scan);
+    	}  	
+	}
 	
+//	private static void goToSavings(BufferedReader scan) {
+//		Sections.fourthSection();
+//		
+//		System.out.println("YOUR CURRENT BALANCE IS: "+balance);//need to write code for balance
+//		int txt = scan.nextInt();
+//	    switch (txt) {
+//	    case 1://deposit
+//	    	System.out.println("You have selected to Make a Deposit");
+//	    	goToSavings();
+//	    	break;
+//	    case 2://withdraw
+//	    	System.out.println("You have selected to Make a Withdrawl");
+//	    	goToChecking();
+//	    	break;
+//	    case 3://transaction history
+//	    	System.out.println("You have selected to Review Transaction History");
+//	    	createAccount();
+//	    	break;
+//	    case 4: //close account
+//	    	System.out.println("You have selected to Close an Account");
+//	    	break;
+//	    case 5://main menu
+//	    	System.out.println("You have selected to Return to Main Menu");
+//	    	userMainMenu(null); //-----------------------------------------------need to redirect with actual param
+//	    	break;
+//	    case 6://exit 
+//	    	System.out.println("You have selected to Exit the Application. Goodbye!");
+//	    	break;
+//	    default:
+//	    	System.out.println("Please select a number 1 through 4");
+//	    }
+//		
+//	}
 	
+//	private static void goToChecking(BufferedReader scan) {
+//		Sections.fifthSection();
+//		balance = get
+//				
+//		System.out.println("YOUR CURRENT BALANCE IS: "+balance);//need to write code for balance
+//		int txt = scan.nextInt();
+//	    switch (txt) {
+//	    case 1://deposit
+//	    	System.out.println("You have selected to Make a Deposit");
+//	    	goToSavings();
+//	    	break;
+//	    case 2://withdraw
+//	    	System.out.println("You have selected to Make a Withdrawl");
+//	    	goToChecking();
+//	    	break;
+//	    case 3://transaction history
+//	    	System.out.println("You have selected to Review Transaction History");
+//	    	createAccount();
+//	    	break;
+//	    case 4: //close account
+//	    	System.out.println("You have selected to Close an Account");
+//	    	break;
+//	    case 5://main menu
+//	    	System.out.println("You have selected to Return to Main Menu");
+//	    	userMainMenu(null); //-----------------------------------------------need to redirect with actual param
+//	    	break;
+//	    case 6://exit 
+//	    	System.out.println("You have selected to Exit the Application. Goodbye!");
+//	    	break;
+//	    default:
+//	    	System.out.println("Please select a number 1 through 4");
+//	    }
+//	}
 	//Manager Methods
 	
-//	//getUserAccountsByLogin --Gets account 
-//	   
-	private static void managerLogin(BufferedReader scan) {
+//getUserAccountsByLogin --Gets account 
+  
+	private static void managerLogin(BufferedReader scan) throws IOException {
 		System.out.println("Please Enter Manager Username");
 		String userMan = scan.readLine();
 		System.out.println("Please Enter Manager Password");
 		String passMan = scan.readLine();
 		
-		
+		String x = ("/Users/Em/Desktop/Pro0/DeborahEmily/project0/src/main/java/com/revature/project/main/Connections.properties");
 		Properties prop = new Properties();
-		InputStream file = null;
-		  
-		 
-		      
-			  file = new FileInputStream("/Users/Em/Desktop/Pro0/DeborahEmily/project0/src/main/java/com/revature/project/main/Connections.properties");
+		InputStream in = new FileInputStream(x);
+		prop.load(in);
 		     
-			  prop.load(file);
-		     
-		      manager = prop.getProperty("Manager");
-		      manpass = prop.getProperty("Password");
+		String manager = prop.getProperty("Manager");
+		String manpass = prop.getProperty("Password");
 		  
-		      if(userMan.equals(manager) && passMan.contentEquals(manpass)) {
+		if(userMan.equals(manager) && passMan.contentEquals(manpass)) {
+		    	  managerMainMenu(scan);
+		      } else {
+		    	  System.out.println("Invalid Credentials Please Type 0 to Try Again or 1 to Main Menu");
+		    	  int back = Integer.parseInt(scan.readLine());
+		    	  if(back == 0) {
+		    		  managerLogin(scan);
+		    	  } else if (back == 1) {
+		    		  try {
+						userEntry(scan);
+					} catch (NumberFormatException e) {
+						e.printStackTrace();
+					} catch (UserNotFoundException e) {
+						e.printStackTrace();
+					}
+		    	  }
+		      }
 				  
-	
-		
-
+	}
 	
 	
-	private static void managerMainMenu(BufferedReader scan) {
+	private static void managerMainMenu(BufferedReader scan) throws IOException {
 		Sections.seventhSection();
 		int txt = Integer.parseInt(scan.readLine());
 	    switch (txt) {
@@ -323,11 +265,34 @@ public class Access {
 	    		}
 	    case 3: //create user
 	    	System.out.println("You have selected to Create a New User");
-	    	createNewUser();
+	    	
+	    	
+	    	System.out.println("Enter New User Username");
+	    	String newUser = scan.readLine();
+	    	System.out.println("Enter New User Password");
+	    	
+	    	int newPass = Integer.parseInt(scan.readLine());
+	    	System.out.println("The New User Username and Password are: {"+newUser+ "} {" +newPass+ "}is this correct? 1. Yes or 2. No");
+	    	int checking = Integer.parseInt(scan.readLine());
+	    	if(checking == 1) {
+	    		BankUser bu = new BankUser(newUser, newPass);
+	    		bud.createUser(bu);
+	    		System.out.println("New User Has Been Created");
+	    		System.out.println("Select 1 to Return to Main Menu or 2 to Exit Application");
+	    		int out = Integer.parseInt(scan.readLine());
+	    		if(out == 1) {
+	    			managerMainMenu(scan);
+	    		} else if (out == 2) {
+	    			System.out.println("You have exited the application -- Goodbye");
+	    			scan.close();
+	    		}
+	    	}else {
+	    		managerMainMenu(scan);
+	    	}
 	    	break;
 	    case 4: //update user
 	    	System.out.println("You have selected to Update an Existing User");
-	    	updateExistingUser();
+	    	//updateExistingUser();
 	    	break;
 	    case 5://delete user
 	    	System.out.println("You have selected to Delete an Existing User");
@@ -338,9 +303,14 @@ public class Access {
 			System.out.println("***************  1. YES       2. NO  ******************");
 			int yn = Integer.parseInt(scan.readLine());
 			if (yn == 1) { 
-				bud.deleteUser(userId);
+				try {
+					bud.deleteUser(userId);
+				} catch (UserNotFoundException e) {
+					e.printStackTrace();
+				}
 				System.out.println("USER HAS BEEN DELETED");
 				System.out.println("TYPE 0 TO RETURN TO MENU");
+				
 		    	int txt3 = Integer.parseInt(scan.readLine());
 		    	if (txt3 == 0) {
 		    		managerMainMenu(scan);
@@ -351,6 +321,7 @@ public class Access {
 	    	break;
 	    case 6://exit application
 	    	System.out.println("You have selected to Exit the Application. Goodbye!");
+	    	scan.close();
 	    	break;
 	    default:
 	    	System.out.println("Please select select a number 1 through 5");
@@ -358,62 +329,6 @@ public class Access {
 	    
 	}
 
-	// Manager Selected Methods
-	
-	private static void viewUserInfo() {
-		bud.getAllUsers();
-	}
-	
-	private static void createNewUser() {
-		System.out.println("Please Enter New Username For User");
-		String newUser = scan.nextLine();
-		System.out.println("***********IS THIS THE CORRECT USERNAME SELECTED *******");
-		System.out.println("*********************"+newUser+"************************");
-		System.out.println("***************  1. YES       2. NO  ******************");
-		int yn = scan.nextInt();
-		if (yn == 1) { 
-			
-			
-			//intiate delete call to BankUser delete
-			
-		}else {
-			managerMainMenu(null);//------------------------------------------verification param
-		}
-		
-	}
-	
-//	private static void updateExistingUser() {//------------------------------need to create an update method
-//		System.out.println("Please Enter User ID");
-//		String userId = scan.nextLine();
-//		System.out.println("*******ARE YOU SURE YOU WANT TO UPDATE THIS USER*******");
-//		System.out.println("*********************"+userId+"************************");
-//		System.out.println("***************  1. YES       2. NO  ******************");
-//		int yn = scan.nextInt();
-//		if (yn == 1) { //intiate delete call to update username/password etc
-//			
-//		}else {
-//			managerMainMenu(null);//------------------------------------------verification param
-//		}
-//		
-//	}
-	
-	private static void deleteExistingUser(BufferedReader scan) {
-		System.out.println("Please Enter User ID");
-		int userId = Integer.parseInt(scan.readLine());
-		System.out.println("*******ARE YOU SURE YOU WANT TO DELETE THIS USER*******");
-		System.out.println("*********************"+userId+"************************");
-		System.out.println("***************  1. YES       2. NO  ******************");
-		int yn = Integer.parseInt(scan.readLine());
-		if (yn == 1) { 
-			bud.deleteUser(userId);
-			System.out.println("USER HAS BEEN DELETED");
-			
-			managerMainMenu(scan);
-		}else {
-			managerMainMenu(scan);
-		}
-		
-	}
 }
 	
 	
